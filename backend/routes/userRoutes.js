4ada030997524983abaff381f1c8d81af4c7df7e
meza.jack <jack.meza.hinostroza@gmail.com>
2023-07-16 12:17:48 -0500

    cambio

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { obtenerDoctor, obtenerDoctores } from '../controllers/doctorController.js';
-
+// indicaciones
 import { 
   autenticar, 
   editarPerfil, 
